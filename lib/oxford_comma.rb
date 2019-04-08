fb6b8812623f685048c1b595b2e2af3f1f56815d
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-08 20:40:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,4 @@
 def oxford_comma(array)
-  names = ["kiwi", "durian", "starfruit", "mangos", "dragon fruit" , "lychees", "pomelos"]
-  kiwi = name[0]
-  kiwi
+  names = ["kiwi", "durian", "starfruit", "mangos", "dragon fruit" , "lychees", "pomelos"].join
   
 end
