0c3fa611964ca5cc6e4915f9ad68975301c78ca1
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-08 20:50:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,5 +2,8 @@ def oxford_comma(array)
   if array.size == 1 
     return array[1]
     elsif array.size == 1 && 2
+    return array.join (" and ")
+    elsif array.size == 1 && 2 && 3
+    return array
 end
 end 
